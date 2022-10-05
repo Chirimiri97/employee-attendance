6a9badeee3d84819cd94f4e4b83e3a48c3f8bbd5
Chirimiri97 <jainch1997@gmail.com>
2022-10-06 00:30:03 +0530

    Added auto sequence for employee_id and defined schema for employees.

--- models/employee.model.js
+++ models/employee.model.js
@@ -1,17 +1,35 @@
 const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 const employeeSchema = new mongoose.Schema(
     {
-        employee_id: {
-            type: Number,
-            unique: true,
+        employee_name: { 
+            type: String, 
+            required: true 
         },
-        employee_name: String,
+
+        employee_date_of_joining: {
+            type: Date, 
+            required: true,
+        },
+        
+        employee_date_of_termination: {
+            type: Date,
+            required: false,
+            default: "",
+        },
+
+        employee_status: {
+            type: Boolean,
+            default: false,
+        },
     },
     {
         timestamps: true,
     }
 );
+
+employeeSchema.plugin(AutoIncrement, { inc_field: "employee_id" });
 
 module.exports = mongoose.model("employees", employeeSchema);
 
